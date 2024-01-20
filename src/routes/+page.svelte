5f4a2eea8d9426349fd8207fb0ab054472b3e1f3
nandanpi <nandanrp09@gmail.com>
2024-01-20 22:12:27 +0530

    geolocation test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,17 @@
-<!-- <div class="bg-[#fad6ca] text-[#2a4fb2]">
-	<div
-		class="flex justify-between fixed w-full py-6 sm:px-8 px-2 font-semibold bg-[#fad6ca] items-center"
-	>
-		<a href="https://finiteloop.co.in" target="_blank">
-			<span class="flex items-center gap-x-2">
-				<img src="/flc_logo_crop.png" alt="flc_Logo" class="md:h-16 h-12" />
-				<h3 class="sm:text-2xl text-md font-bold">Finite Loop Club</h3>
-			</span>
-		</a>
-		<a href="https://www.instagram.com/p/C2ES0MOv7tK/" target="_blank">
-			<h3 class="sm:text-2xl text-md font-bold">HackLoop</h3>
-		</a>
-	</div>
-	<div class="flex items-center justify-center w-full min-h-screen pt-32">
-		<div class="w-[80%] justify-center space-y-8">
-			<h1 class="md:text-6xl text-3xl font-bold">About this project</h1>
-
-			<h3 class="md:text-4xl text-2xl font-semibold">Problem Statement</h3>
-			<p class="max-w-4xl text-justify md:text-xl text-md">
-				In today's fast-paced world, staying informed about local events such as accidents, traffic
-				jams, and other incidents is crucial for individuals to navigate their surroundings
-				efficiently. When it comes locally, to a place like Mangalore, to notify people about
-				potholes, random road construction, unmarked speed breakers etc. To address this need,
-				develop a user-friendly solution that allows users to address and check for real-time local
-				events happening in their vicinity.
-			</p>
-
-			<h3 class="md:text-4xl text-2xl font-semibold">Solution</h3>
-			<ul class="max-w-4xl text-justify md:text-xl text-md list-disc ml-10">
-				<li>Feed that shows all local events (accidents/traffic jam etc) in a particular area.</li>
-				<li>Users can also report an event that they have witnessed.</li>
-				<li>Satellite map with verified traffic tags</li>
-			</ul>
-
-			<h3 class="md:text-4xl text-2xl font-semibold">Major Problems to be solved</h3>
-			<ul class="max-w-4xl text-justify md:text-xl text-md list-disc ml-10">
-				<li>
-					Verifying authenticity of users <span class="font-semibold"
-						>(Solution: Using Meri Pehchaan API)</span
-					>
-				</li>
-				<li>Verifying authenticity of posts</li>
-			</ul>
-		</div>
-	</div>
-</div> -->
-
-<script>
+<script lang="ts">
 	import { signIn, signOut } from '@auth/sveltekit/client';
 	import { page } from '$app/stores';
+	import Geolocation from 'svelte-geolocation';
+	let coords: [number, number] = [0, 0];
 </script>
 
-<h1>SvelteKit Auth Example</h1>
-<p>
+<Geolocation getPosition bind:coords />
+<p class="flex flex-col gap-5">
+	<span>{coords[0]}</span>
+	<span>{coords[1]}</span>
 	{#if $page.data.session}
 		{#if $page.data.session.user?.image}
-			<span style="background-image: url('{$page.data.session.user.image}')" class="avatar" />
+			<img src={$page.data.session.user.image} alt="avatar" class="rounded-full" />
 		{/if}
 		<span class="signedInText">
 			<small>Signed in as</small><br />
